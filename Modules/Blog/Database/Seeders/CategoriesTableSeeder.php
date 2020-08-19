118348c830547d7614bd8848d1bf04dc00ee73b0
Damian B <damianbialkowski8@gmail.com>
2020-08-19 19:06:30 +0200

    Blog module ver.0.2 pre

--- Modules/Blog/Database/Seeders/CategoriesTableSeeder.php
+++ Modules/Blog/Database/Seeders/CategoriesTableSeeder.php
@@ -25,13 +25,10 @@ class CategoriesTableSeeder extends Seeder
         ];
 
         foreach ($categories as $category) {
-<<<<<<< HEAD
             $categoryExists = Category::where('name', $category)->first();
             if ($categoryExists) {
                 continue;
             }
-=======
->>>>>>> origin/master
             Category::create([
                 'name' => $category,
                 'description' => 'description of ' . $category,
--- Modules/Blog/Entities/Article.php
+++ Modules/Blog/Entities/Article.php
@@ -5,7 +5,6 @@ namespace Modules\Blog\Entities;
 use Modules\Cms\Entities\CmsModel;
 use Spatie\MediaLibrary\HasMedia\HasMedia;
 use Spatie\MediaLibrary\HasMedia\HasMediaTrait;
-<<<<<<< HEAD
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Cviebrock\EloquentSluggable\Sluggable;
 use Modules\Cms\Traits\CmsTrait;
@@ -16,12 +15,6 @@ class Article extends CmsModel implements HasMedia
         SoftDeletes,
         Sluggable,
         CmsTrait;
-=======
-
-class Article extends CmsModel implements HasMedia
-{
-    use HasMediaTrait, SoftDeletes;
->>>>>>> origin/master
 
     protected $fillable = [
         'domain_id',
@@ -36,7 +29,6 @@ class Article extends CmsModel implements HasMedia
         'published_at',
     ];
 
-<<<<<<< HEAD
     public function sluggable()
     {
         return [
@@ -51,19 +43,14 @@ class Article extends CmsModel implements HasMedia
         return 'slug';
     }
 
-=======
->>>>>>> origin/master
     public function categories()
     {
         return $this->belongsToMany(Category::class);
     }
 
-<<<<<<< HEAD
     public function user()
     {
         return $this->belongsTo(Admin::class);
     }
 
-=======
->>>>>>> origin/master
 }
